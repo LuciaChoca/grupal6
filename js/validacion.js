b45c0996cbdbaba9befc0b7595aea0bea676687c
Emanuel Overlay Rodriguez <emanueloverlay@outlook.com>
2022-10-19 19:19:38 -0300

    update pass2

--- js/validacion.js
+++ js/validacion.js
@@ -3,11 +3,15 @@
   let forms = document.querySelectorAll(".needs-validation");
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener("submit", function (event) {
-          if (checkPass() && !form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation();
-          }
-          form.classList.add("was-validated");
+            if (!form.checkValidity()){
+              event.preventDefault()
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+            if (checkPass ()) {
+              console.log("validado")
+            }
+
       },
       false
     );
@@ -17,12 +21,7 @@
 function checkPass(){
   const pass1 = document.getElementById("password1");
   const pass2 = document.getElementById("password2");
-      if (pass1.value != pass2.value) {
-        console.log("no")
-          return false;
-      } else {
-        console.log("si")
-          return true;
-      }
+
+  return pass1.value == pass2.value
 }
 
