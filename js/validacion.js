a7f838b03d67cefb5920dc8864168e8102a5a140
Emanuel Overlay Rodriguez <emanueloverlay@outlook.com>
2022-10-19 21:08:38 -0300

    update checkbox

--- js/validacion.js
+++ js/validacion.js
@@ -8,8 +8,8 @@
               event.stopPropagation();
             }
             form.classList.add("was-validated");
-            if (checkPass ()) {
-              console.log("validado")
+            if (checkPass() && form.checkValidity() && checkbox()) {
+              console.log("formulario validado")
             }
 
       },
@@ -20,7 +20,33 @@
 
 function checkPass(){
   const pass1 = document.getElementById("password1");
+  const valorpass = document.getElementById("password1").value.length;
   const pass2 = document.getElementById("password2");
+  if (valorpass > 6) {
+    pass1.classList.remove("form-novalido");
+    pass1.classList.add("form-valido");
+  } else {
+    pass1.classList.remove("form-valido");
+    pass1.classList.add("form-novalido");
+  }
   return pass1.value == pass2.value
 }
 
+function passIguales(){
+  const pass2 = document.getElementById("password2");
+  if (!checkPass()){
+    pass2.classList.remove("form-valido");
+    pass2.classList.add("form-novalido");
+  } else {
+    pass2.classList.remove("form-novalido");
+    pass2.classList.add("form-valido");
+  }
+}
+
+function checkbox (){
+  let checkbox = document.getElementById("terminos");
+  if (!checkbox.checked) {
+    checkbox.classList.add("form-check-input-valid");
+  }
+checkbox.checked;
+}
