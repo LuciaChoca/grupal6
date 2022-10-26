d1783696ef7ba3d51d5c99646157365bc3f03565
camilaabarno <camilabarno@hotmail.com>
2022-10-26 18:56:57 -0300

    c

--- js/validacion.js
+++ js/validacion.js
@@ -3,6 +3,7 @@
   let forms = document.querySelectorAll(".needs-validation");
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener("submit", function (event) {
+      checkbox();
             if (!form.checkValidity()){
               event.preventDefault()
               event.stopPropagation();
@@ -22,7 +23,7 @@ function checkPass(){
   const pass1 = document.getElementById("password1");
   const valorpass = document.getElementById("password1").value.length;
   const pass2 = document.getElementById("password2");
-  if (valorpass > 6) {
+  if (valorpass >= 6) {
     pass1.classList.remove("form-novalido");
     pass1.classList.add("form-valido");
   } else {
@@ -44,9 +45,18 @@ function passIguales(){
 }
 
 function checkbox (){
+  let terminosLink = document.getElementById("terminosLink");
   let checkbox = document.getElementById("terminos");
-  if (!checkbox.checked) {
-    checkbox.classList.add("form-check-input-valid");
+  if (checkbox.checked) {
+    checkbox.classList.remove("is-invalid");
+    checkbox.classList.add("is-valid");
+    terminosLink.classList.remove("is-invalid");
+    terminosLink.classList.add("is-valid");
+  }else{
+    checkbox.classList.remove("is-valid");
+    checkbox.classList.add("is-invalid");
+    terminosLink.classList.remove("is-valid");
+    terminosLink.classList.add("is-invalid");
   }
-checkbox.checked;
+return checkbox.checked;
 }
